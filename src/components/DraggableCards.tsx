da1201f23ace62b88b6697ac2a438ee240942742
oveddan <stangogh@gmail.com>
2020-03-31 21:45:54 -0700

    Refactored draggable cards in the following way: * Split DraggableCard into itself and a Smaller component that is only rendered when the cards to show have changed.  This is determined by the matrixX and matrixY, which only changes every x often, determined by the draggable screen current. * Only cards are rendered that are within x card positions of the current matrix x and y * StudentCards dont worry about visibility - they just are rendered if they are part of the viewable grid. * Wrapped functions in React.memo - which prevents them from being rendered unless their props change. * Split each state value into its own useState/useEffect so that each state value can be updated independently

--- src/components/DraggableCards.tsx
+++ src/components/DraggableCards.tsx
@@ -12,16 +12,6 @@ interface IStudentsProps extends RouteComponentProps {
   students?: IStudentSummary[];
 }
 
-interface IStudentCardProps {
-  student: IStudentSummary;
-  springValue: SpringValue<number[]>;
-  xy: number[];
-  wh: number[];
-  offsets: number[];
-  id: string;
-  onChange: any;
-}
-
 interface IDragableCardsState {
   inViewPort: number[][];
   studentsMatrix: number[][];
@@ -65,168 +55,201 @@ const isInViewPort = (
   else return true;
 };
 
-const DraggableCards = ({ students }: IStudentsProps) => {
-  const getOffset = (xy: number[], cardSize: number[]): number[] =>
-    scaleVector(multiplyElementWise(xy, cardSize), -1);
-  const getProjectId = (xy: number[], shape: number[]): string =>
-    `project${xy[0] * shape[0] + xy[1]}`;
-  const cardSize: number[] = [240, 360];
-  // the shape of the matrix. However if I have a matrix, that means this is still fake infinite.
-  // There're 1 million nodes in this matrix, and the performance is still good. So this is acceptable.
-  const shape: number[] = [400, 400];
-  const start: number[] = scaleVector(shape, 0.5);
+// the shape of the matrix. However if I have a matrix, that means this is still fake infinite.
+// There're 1 million nodes in this matrix, and the performance is still good. So this is acceptable.
+const matrixShape: number[] = [800, 800];
+const cardSize: number[] = [240, 360];
+const windowSizeInCards = [14, 6];
 
-  const coolDown = new CoolDown(200);
-
-  const initializeMatrix = () => {
-    let r = [];
-    if (students) {
-      for (let i = 0; i < shape[0]; i++) {
-        r.push(
-          Array.from({ length: shape[1] }, () =>
-            Math.floor(Math.random() * students!.length)
-          )
-        );
-      }
+const initializeMatrix = (
+  students: IStudentSummary[] | undefined
+): number[][] => {
+  const r: number[][] = [];
+  if (students) {
+    for (let i = 0; i < matrixShape[0]; i++) {
+      r.push(
+        Array.from({ length: matrixShape[1] }, () =>
+          Math.floor(Math.random() * students!.length)
+        )
+      );
     }
-    return r;
-  };
+  }
+  return r;
+};
 
-  const [{ inViewPort, studentsMatrix }, setState] = useState<
-    IDragableCardsState
-  >({ inViewPort: [start], studentsMatrix: initializeMatrix() });
-  const [{ pos }, set] = useSpring(() => ({
-    pos: multiplyElementWise(start, cardSize),
-  }));
+interface ICardsProps {
+  students: IStudentSummary[];
+  matrixX: number;
+  matrixY: number;
+}
+
+interface CardToShow {
+  student: IStudentSummary;
+  matrixX: number;
+  matrixY: number;
+}
+
+const halfWindowSizeInCards = scaleVector(windowSizeInCards, 0.5) as [
+  number,
+  number
+];
+
+const getCardsInMatrixToShow = (
+  matrixX: number,
+  matrixY: number,
+  studentsMatrix: number[][],
+  students: IStudentSummary[]
+): CardToShow[] => {
+  const startX = matrixX - halfWindowSizeInCards[0];
+  const endX = matrixX + halfWindowSizeInCards[0];
+  const startY = matrixY - halfWindowSizeInCards[1];
+  const endY = matrixY + halfWindowSizeInCards[1];
+
+  const result: CardToShow[] = [];
+
+  for (let x = startX; x < endX; x++) {
+    for (let y = startY; y < endY; y++) {
+      result.push({
+        student: students[studentsMatrix[x][y]],
+        matrixX: x,
+        matrixY: y,
+      });
+    }
+  }
+
+  return result;
+};
+
+const Cards = React.memo(({ students, matrixX, matrixY }: ICardsProps) => {
+  const [studentsMatrix, setStudentMatrix] = useState<number[][]>();
+
+  const [inViewPortList, setInViewportList] = useState<CardToShow[]>([]);
+
+  useEffect(() => {
+    if (!studentsMatrix) return;
+    setInViewportList(
+      getCardsInMatrixToShow(matrixX, matrixY, studentsMatrix, students)
+    );
+  }, [matrixX, matrixY, studentsMatrix, students]);
+
+  useEffect(() => {
+    setStudentMatrix(initializeMatrix(students));
+  }, [students]);
+
+  if (!studentsMatrix) return null;
+
+  return (
+    <>
+      {inViewPortList.map(({ student, matrixX: x, matrixY: y }) => {
+        /* Since Each Student Card is wrapped in React.memo - it will only be re-rendered when matrixXy values change */
+        return (
+          <StudentCard
+            key={`${student.student_id}_${x}_${y}`}
+            student={student}
+            matrixX={x}
+            matrixY={y}
+          />
+        );
+      })}
+    </>
+  );
+});
+
+const toPositionInMatrix = ([centerX, centerY]: [number, number]): [
+  number,
+  number
+] => {
+  const [cardWidth, cardHeight] = cardSize;
+  return [Math.floor(centerX / cardWidth), Math.floor(centerY / cardHeight)];
+};
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+const DraggableCards = ({ students }: IStudentsProps) => {
+  // const coolDown = new CoolDown(200);
+  const windowSize = multiplyElementWise(matrixShape, cardSize) as [
+    number,
+    number
+  ];
+  const [startX, startY] = scaleVector(windowSize, 0.5);
+
+  const [position, set] = useSpring<Position>(() => ({ x: startX, y: startY }));
+
+  const [matrixXy, setMatrixCenterXy] = useState<[number, number]>(
+    toPositionInMatrix([startX, startY])
+  );
+
   const bind = useDrag(
     ({ down, movement: xy, velocity, direction }) => {
-      // console.log(xy);
       set({
-        pos: xy,
+        x: xy[0],
+        y: xy[1],
         config: {
           velocity: scaleVector(direction, velocity * 1000),
           decay: false,
         },
       });
+
+      setMatrixCenterXy(toPositionInMatrix(xy as [number, number]));
     },
     {
-      initial: () => pos.getValue() as any,
+      initial: () => [startX, startY],
     }
   );
 
-  useEffect(() => {}, []);
-
-  const onChange = (isVisible: boolean, id: string, xy: number[]) => {
-    if (coolDown.queue.length === 0) coolDown.queue = inViewPort.slice();
-    if (isVisible) {
-      const neighbors = getUnvisitedNeighbors(coolDown.queue, xy, shape);
-      coolDown.queue = coolDown.queue.concat(neighbors);
-    }
-    for (let i = coolDown.queue.length - 1; i >= 0; i--) {
-      const element = coolDown.queue[i];
-      if (
-        !isInViewPort(getOffset(element, cardSize), pos.getValue(), cardSize, [
-          500,
-          500,
-        ])
-      ) {
-        coolDown.queue.splice(i, 1);
-      }
-    }
-    // console.log(document.querySelectorAll(`.student-card`)?.length);
-    console.log(inViewPort.length, coolDown.queue.length);
-    coolDown.excuteSetState = () => {
-      console.log("callling");
-      if (!coolDown.ready()) {
-        if (!coolDown.timerIsOn) {
-          clearTimeout(coolDown.timeOut);
-          coolDown.timeOut = setTimeout(() => {
-            coolDown.excuteSetState();
-          }, 200);
-          coolDown.timerIsOn = true;
-        }
-      } else {
-        clearTimeout(coolDown.timeOut);
-        setState((prevState) => ({
-          inViewPort: coolDown.queue,
-          studentsMatrix: prevState.studentsMatrix,
-        }));
-        coolDown.setNow();
-        coolDown.timerIsOn = false;
-      }
-    };
-    coolDown.excuteSetState();
-  };
+  if (!students) return null;
 
   return (
     <>
-      <div id="canvas-invisible-height" />
-      <div {...bind()} id="projects-canvas" style={{}}>
-        {inViewPort.map((xy: number[], index: number) => {
-          return (
-            <StudentCard
-              id={getProjectId(xy, shape)}
-              key={index}
-              student={students![studentsMatrix[xy[0]][xy[1]]]}
-              springValue={pos}
-              xy={xy}
-              wh={cardSize}
-              offsets={getOffset(xy, cardSize)}
-              onChange={onChange}
-            />
-          );
-        })}
-      </div>
+      <animated.div
+        {...bind()}
+        style={{
+          position: "relative",
+          width: windowSize[0],
+          height: windowSize[1],
+          ...position,
+        }}
+      >
+        {/* Since Cards are wrapped in React.memo - they will only be re-rendered when matrixXy values change */}
+        <Cards
+          matrixX={matrixXy[0]}
+          matrixY={matrixXy[1]}
+          students={students}
+        />
+      </animated.div>
     </>
   );
 };
 
-const StudentCard = ({
-  id,
-  student,
-  springValue,
-  xy,
-  wh,
-  offsets,
-  onChange,
-}: IStudentCardProps) => {
-  let count = 0;
-  const localOnChange = (visibility: boolean) => {
-    onChange(visibility, id, xy);
-  };
-  return (
-    <div id={id}>
-      <animated.div
+const getOffset = (xy: number[], cardSize: number[]): number[] =>
+  scaleVector(multiplyElementWise(xy, cardSize), -1);
+
+interface IStudentCardProps {
+  student: IStudentSummary;
+  matrixX: number;
+  matrixY: number;
+}
+
+const StudentCard = React.memo(
+  ({ student, matrixX: x, matrixY: y }: IStudentCardProps) => {
+    const offsets = getOffset([x, y], cardSize);
+    return (
+      <div
         className="student-card shadow"
         style={{
-          transform: to(
-            [springValue],
-            ([x, y]) => `translate3d(${x}px,${y}px,0)`
-          ),
-          width: `${wh[0] - 20}px`,
-          height: `${wh[1] - 20}px`,
+          position: "absolute",
+          width: `${cardSize[0] - 20}px`,
+          height: `${cardSize[1] - 20}px`,
           left: `${offsets[0]}px`,
           top: `${offsets[1]}px`,
           backgroundImage: `url(${student.portfolio_icon.src})`,
         }}
-      >
-        <VisibilitySensor
-          onChange={localOnChange}
-          partialVisibility={true}
-          delayedCall={true}
-        >
-          <div
-            style={{
-              width: `${wh[0] - 20}px`,
-              height: `${wh[1] - 20}px`,
-              left: `0px`,
-              top: `0px`,
-            }}
-          ></div>
-        </VisibilitySensor>
-      </animated.div>
-    </div>
-  );
-};
+      ></div>
+    );
+  }
+);
 
 export default DraggableCards;
