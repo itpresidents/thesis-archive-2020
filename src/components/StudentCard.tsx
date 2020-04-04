497086f312d8324e91d72741c4499c87d48c77a7
YangYang <yy2473@nyu.edu>
2020-04-04 16:58:47 -0400

    solved conflict bettween dragging and link

--- src/components/StudentCard.tsx
+++ src/components/StudentCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { IStudentSummary } from "../types";
 import { cardSize } from "../config";
 import { scaleVector, multiplyElementWise } from "util/vector";
@@ -18,6 +18,25 @@ const StudentCard = React.memo(
     const offsets = getOffset([x, y], cardSize);
     const width = `${cardSize[0] - 24}px`;
     const height = `${cardSize[1] - 120}px`;
+    const linkRef = useRef<null | HTMLAnchorElement | any>(null);
+    const preventDefault = (e: React.FormEvent<HTMLAnchorElement>): void => {
+      console.log("prevent default");
+      e.preventDefault();
+    };
+    let clickStartXy = [0, 0];
+    const onMouseDown = (e: React.MouseEvent) => {
+      if (!linkRef.current) return;
+      clickStartXy = [e.clientX, e.clientY];
+    };
+    const onMouseUp = (e: React.MouseEvent) => {
+      if (!linkRef.current) return;
+      const dist = Math.hypot(
+        e.clientX - clickStartXy[0],
+        e.clientY - clickStartXy[1]
+      );
+      if (dist > 10) linkRef.current!.addEventListener("click", preventDefault);
+      else linkRef.current!.removeEventListener("click", preventDefault);
+    };
     return (
       <div
         className="student-card position-absolute"
@@ -31,7 +50,12 @@ const StudentCard = React.memo(
           })`,
         }}
       >
-        <Link to={`/students/${student.student_id}`}>
+        <Link
+          to={`/students/${student.student_id}`}
+          ref={linkRef}
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+        >
           <div
             style={{
               height: height,
