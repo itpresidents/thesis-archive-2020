a0c5c9e2b5dbd5092ba70d044be7b8a7a65add2a
YangYang <yy2473@nyu.edu>
2020-04-05 17:32:24 -0400

    move Nav to its own component file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,11 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "@reach/router";
 import HeaderBG from "./HeaderBG";
+import NavBar from "./NavBar";
 
 const Header = () => (
   <>
     <HeaderBG />
-    <div className="sticky-top bg-white border-top border-bottom">
-      <Navbar>
-        <Navbar.Brand>Thesis Archive 2020</Navbar.Brand>
-        <Nav fill variant="tabs">
-          <Nav.Item>
-            <Link to="/" className="nav-link">
-              Students
-            </Link>{" "}
-          </Nav.Item>
-          <Nav.Item>
-            <Link to="/videos" className="nav-link">
-              Videos
-            </Link>{" "}
-          </Nav.Item>
-        </Nav>
-      </Navbar>
-    </div>
+    <NavBar />
   </>
 );
 
