3379d7d62048f4d29e2c31b86f1699bd7f63b9af
oveddan <stangogh@gmail.com>
2020-04-05 17:49:50 -0700

    styled the navbar according to specs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,49 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "@reach/router";
+import { Link, LinkProps } from "@reach/router";
 import HeaderBG from "./HeaderBG";
+import cx from "classnames";
+
+const NavLink = <TState extends {}>(props: LinkProps<TState>) => {
+  return (
+    <Link
+      to={props.to}
+      getProps={({ isCurrent }) => ({
+        class: cx("nav-link", { active: isCurrent }),
+      })}
+    >
+      {props.children}
+    </Link>
+  );
+};
 
 const Header = () => (
   <>
     <HeaderBG />
-    <div className="sticky-top bg-white border-top border-bottom">
-      <Navbar>
-        <Navbar.Brand>Thesis Archive 2020</Navbar.Brand>
-        <Nav fill variant="tabs">
+    <Navbar
+      expand="md"
+      sticky="top"
+      className="border-top border-bottom"
+      bg="white"
+    >
+      <Link to="/" className="navbar-brand">
+        Thesis Archive 2020
+      </Link>
+      <Navbar.Toggle />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="justify-content-end ml-md-auto" as="ul">
           <Nav.Item>
-            <Link to="/" className="nav-link">
-              Students
-            </Link>{" "}
+            <NavLink to="/">Explore</NavLink>{" "}
           </Nav.Item>
           <Nav.Item>
-            <Link to="/videos" className="nav-link">
-              Videos
-            </Link>{" "}
+            <NavLink to="/videos">Watch</NavLink>{" "}
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink to="/about">About</NavLink>{" "}
           </Nav.Item>
         </Nav>
-      </Navbar>
-    </div>
+      </Navbar.Collapse>
+    </Navbar>
   </>
 );
 
