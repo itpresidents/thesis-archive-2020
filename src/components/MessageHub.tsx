d34903f5680d3fcc07989231ac4c0c32bb2cafb8
YangYang <yy2473@nyu.edu>
2020-04-07 19:12:25 -0400

    type safe now

--- src/components/MessageHub.tsx
+++ src/components/MessageHub.tsx
@@ -1,20 +1,28 @@
-// @ts-nocheck
-import React, { useState, useEffect } from "react";
-import { useTransition, animated, config } from "react-spring";
+import React, { useRef, useState, useEffect } from "react";
+import { useTransition, animated } from "react-spring";
 import { FiX } from "react-icons/fi";
 import "../scss/messageHub.scss";
 
-let id = 0;
+const DEBUG = false;
 
-const DEBUG = true;
+interface IMessage {
+  key: number;
+  msg: string;
+}
 
 const MessageHub = () => {
   const config = { tension: 125, friction: 20, precision: 0.1 };
   const timeout = 3000;
-  const [refMap] = useState(() => new WeakMap());
-  const [cancelMap] = useState(() => new WeakMap());
-  const [items, setItems] = useState([]);
+  const [refMap] = useState<WeakMap<IMessage, HTMLDivElement>>(
+    () => new WeakMap()
+  );
+  const [cancelMap] = useState<WeakMap<IMessage, Function>>(
+    () => new WeakMap()
+  );
+  const [items, setItems] = useState<IMessage[]>([]);
   const [listenerAdded, setListenerAdded] = useState(false);
+  const [key, setKey] = useState<number>(0);
+  const listenerRef = useRef<null | HTMLDivElement | any>(null);
   const transition = useTransition(items, {
     key: (item) => item.key,
     from: { opacity: 0, height: 0, life: "100%", dead: 1 },
@@ -27,11 +35,11 @@ const MessageHub = () => {
       });
       await next({
         opacity: 1,
-        height: refMap.get(item).offsetHeight,
+        height: refMap.get(item) ? refMap.get(item)!.offsetHeight : 48,
         config,
       });
       await next({ life: "0%", config: { duration: timeout } });
-      cancelMap.get(item)();
+      cancelMap.get(item)!();
     },
     leave: (item) => async (next) => {
       if (DEBUG) console.log(`  Leaving:`, item.key);
@@ -40,45 +48,41 @@ const MessageHub = () => {
     },
   });
 
-  const handleMessage = (e) => {
-    setItems((state) => [...state, { key: id++, msg: e.detail.message }]);
-    console.log("items:");
-    console.log(items);
+  const handleMessage = (e: CustomEvent) => {
+    setKey((key) => key++);
+    setItems((state) => [...state, { key, msg: e.detail.message }]);
   };
 
   useEffect(() => {
     if (listenerAdded === false) {
-      document.addEventListener("toMessageHub", (e) => {
+      // ref or other elements don't work, it has to be the document itself, I don't know why.
+      document.addEventListener("toMessageHub", ((e: CustomEvent) => {
         handleMessage(e);
-      });
+      }) as EventListener);
       console.log("added");
       setListenerAdded(true);
     }
   }, []);
 
   return (
-    <div id="message-hub-positioner">
+    <div id="message-hub-positioner" ref={listenerRef}>
       <div id="Container">
         {transition(({ life, dead, ...style }, item) => {
-          console.log(dead.get());
           return dead.get() === 0 ? null : (
-            <animated.div id="Message" key={id} style={style}>
-              <animated.div
-                id="Content"
-                ref={(ref) => ref && refMap.set(item, ref)}
-              >
+            <animated.div id="Message" key={item.key} style={style}>
+              <div id="Content" ref={(ref) => ref && refMap.set(item, ref)}>
                 <animated.div id="messageLife" style={{ right: life }} />
                 <p>{item.msg}</p>
                 <button
                   id="Button"
                   onClick={(e) => {
                     e.stopPropagation();
-                    cancelMap.has(item) && cancelMap.get(item)();
+                    cancelMap.has(item) && cancelMap.get(item)!();
                   }}
                 >
                   <FiX size={24} />
                 </button>
-              </animated.div>
+              </div>
             </animated.div>
           );
         })}
@@ -87,8 +91,8 @@ const MessageHub = () => {
   );
 };
 
-export const AddMessage = (message) => {
-  console.log("dispath");
+export const AddMessage = (message: string) => {
+  console.log("Adding message: ", message);
   document.dispatchEvent(
     new CustomEvent("toMessageHub", {
       detail: {
@@ -98,22 +102,22 @@ export const AddMessage = (message) => {
   );
 };
 
-const CreateTestMessages = () => {
-  const testMessage = () => {
-    AddMessage(Math.random());
-  };
-  return (
-    <>
-      <MessageHub />
-      <div
-        className="w-100 fixed-top text-center"
-        style={{ zIndex: 9999999 }}
-        onClick={testMessage}
-      >
-        Click here to create test notifications
-      </div>
-    </>
-  );
-};
+// const CreateTestMessages = () => {
+//   const testMessage = () => {
+//     AddMessage(Math.random().toString());
+//   };
+//   return (
+//     <>
+//       <MessageHub />
+//       <div
+//         className="w-100 fixed-top text-center"
+//         style={{ zIndex: 9999999 }}
+//         onClick={testMessage}
+//       >
+//         Click here to create test notifications
+//       </div>
+//     </>
+//   );
+// };
 
 export default MessageHub;
