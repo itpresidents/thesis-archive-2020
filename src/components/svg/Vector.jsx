d9721cc5268dbff65cbddf713ba7caebee80542a
oveddan <stangogh@gmail.com>
2020-04-09 17:36:17 -0700

    styled details page with proper spacing

---
+++ src/components/svg/Vector.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Vector = () => (
+  <svg
+    width="11"
+    height="17"
+    viewBox="0 0 11 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 1.08545L9 8.37564L1 15.6106"
+      stroke="#333333"
+      stroke-width="1.38785"
+      stroke-miterlimit="10"
+    />
+  </svg>
+);
+
+export default Vector;
