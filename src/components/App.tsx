dbaea0ed1789a26e1aa9967a925e66347e80b386
YangYang <yy2473@nyu.edu>
2020-04-19 21:04:07 -0400

    removing topic.tsx, topics.tsx and studentCards.tsx.  placing videos, studentDetails and explore into ./src/pages/

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import * as api from "util/api";
 import "scss/styles.scss";
-import Explore from "./Explore";
-import StudentDetails from "./StudentDetails";
+import Explore from "../pages/Explore";
+import StudentDetails from "../pages/StudentDetails";
 import { Switch, Route } from "react-router-dom";
 import { IStudentSummary } from "types";
 import Header from "./Header";
-import Videos from "./Videos";
+import Videos from "../pages/Videos";
 import { Context } from "../util/contexts";
 import useWindowSize from "../util/useWindowSize";
 import { isMobile } from "react-device-detect";
