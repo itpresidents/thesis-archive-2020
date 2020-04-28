80dea54740886dfac5282e483bc188c2866a7908
YangYang <yy2473@nyu.edu>
2020-04-27 20:52:53 -0400

    remove dynamic card size

--- src/util/cardSizeBreakpoints.ts
+++ src/util/cardSizeBreakpoints.ts
@@ -1,6 +1,6 @@
 import { ICardSize } from "types";
 
-const bkp_lg = 992;
+//const bkp_lg = 992;
 const bkp_sm = 576;
 
 // those arbitrary numbers are from the design.
@@ -14,10 +14,8 @@ export const getCardHeightByCardWidth = (cardW: number = 212): ICardSize => ({
 
 export const getCardSizeByWindowSize = (windowSize: number[]): ICardSize => {
   switch (true) {
-    case windowSize[0] > bkp_lg:
-      return getCardHeightByCardWidth(212 + windowSize[0] / 50);
     case windowSize[0] > bkp_sm:
-      return getCardHeightByCardWidth(193 + windowSize[0] / 50);
+      return getCardHeightByCardWidth(212);
     default:
       return getCardHeightByCardWidth(175);
   }
