e6b5688318563dafaa7f502c461760d402ee523e
oveddan <stangogh@gmail.com>
2020-05-08 09:43:48 -0700

    made base url thesis2020

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/thesis2020">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
